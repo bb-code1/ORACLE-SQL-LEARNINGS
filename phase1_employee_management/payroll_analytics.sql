0d5cbc68daae49239d5640223f1142c521844430
bb-code1 <barkatbashir10@gmail.com>
2026-01-06 17:30:00 +0530

    Evaluate payroll averages ignoring null offsets

--- phase1_employee_management/payroll_analytics.sql
+++ phase1_employee_management/payroll_analytics.sql
@@ -17,3 +17,13 @@ SELECT COUNT(*) AS total_records,
        COUNT(email) AS records_with_email,
        COUNT(commission_pct) AS records_with_commission
 FROM employees;
+
+-- Day 6: Aggregate Null Handling
+-- Concept: Aggregate functions (except COUNT(*)) ignore NULLs.
+-- This changes average outcomes depending on whether nulls are transformed to 0 first.
+-- AVG(commission_pct) calculates sum(commission_pct) / count(commission_pct).
+-- AVG(NVL(commission_pct, 0)) calculates sum(commission_pct) / count(*).
+
+SELECT ROUND(AVG(commission_pct), 4) AS average_commission_active_earners,
+       ROUND(AVG(NVL(commission_pct, 0)), 4) AS average_commission_all_employees
+FROM employees;
